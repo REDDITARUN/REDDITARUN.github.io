36e817b350278f468c06e0df1e5b85387bd74308
Tarun Reddi <rbchstarun@gmail.com>
2025-12-05 22:12:16 -0800

    Enable static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
