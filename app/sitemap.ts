39f1c01d7fb46338a9566509308662504981df3b
Tarun Reddi <rbchstarun@gmail.com>
2025-12-05 23:33:18 -0800

    Mark sitemap as static for export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,17 +2,19 @@ import type { MetadataRoute } from 'next'
 import { WEBSITE_URL } from '@/lib/constants'
 import { BLOG_POSTS } from './data'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = WEBSITE_URL
 
   // 1. Static routes
-  const routes = ['', '/blog'].map((route) => ({
+  const routes: MetadataRoute.Sitemap = ['', '/blog'].map((route) => ({
     url: `${base}${route}`,
     lastModified: new Date(),
   }))
 
   // 2. Blog posts
-  const posts = BLOG_POSTS.map((post) => ({
+  const posts: MetadataRoute.Sitemap = BLOG_POSTS.map((post) => ({
     url: `${base}${post.link}`,
     lastModified: new Date(),
   }))
